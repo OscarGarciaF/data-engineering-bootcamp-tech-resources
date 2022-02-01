42196a947f13081950ffe6176651376e62ee7a42
OscarGarciaF <garcia.oscar1729@gmail.com>
2022-02-01 14:22:04 -0600

    make emr file

--- custom_dags/dag_s3_to_EMR.py
+++ custom_dags/dag_s3_to_EMR.py
@@ -9,7 +9,7 @@ import re
 import csv
 
 
-class s3ToGlue(BaseOperator):
+class s3ToEMR(BaseOperator):
     """PostgresToSparkTransfer: custom operator created to move a s3 to glue and back to s3
        Author: Oscar Garcia.      
        Creation Date: 01/02/2022.                   
@@ -32,7 +32,7 @@ class s3ToGlue(BaseOperator):
             aws_conn_postgres_id ='postgres_default',
             aws_conn_id='aws_default',          
             *args, **kwargs):
-        super(s3ToGlue, self).__init__(*args, **kwargs)
+        super(s3ToEMR, self).__init__(*args, **kwargs)
         self.aws_conn_postgres_id  = aws_conn_postgres_id 
         self.aws_conn_id = aws_conn_id
         self.s3_bucket = s3_bucket
